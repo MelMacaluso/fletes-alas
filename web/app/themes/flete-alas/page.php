5dfaf0f6bff99408c6fc73c25e9894dc9c3d43b6
Venom (Mel Macaluso) <capeeshe@gmail.com>
2018-06-08 01:22:57 +0100

    dynamic recursive for pages and helper function added

--- web/app/themes/flete-alas/page.php
+++ web/app/themes/flete-alas/page.php
@@ -1,4 +1,6 @@
 <?php
-
 global $post;
-get_template_part("pages/$post->post_name");
+
+$pages = findPages('/pages', 'php');
+
+in_array($post->post_name, $pages) ? get_template_part("pages/$post->post_name") : get_template_part('page-default');
