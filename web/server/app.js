0b972341afc13361c46837f60c112249f6d35ae3
Venom (Mel Macaluso) <capeeshe@gmail.com>
2018-06-13 00:05:02 +0100

    adding internal server figlio di una stramaledetta puttana di gesu cristo

---
+++ web/server/app.js
@@ -0,0 +1,34 @@
+// Utils 
+require('dotenv').config()
+
+// Server setup
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  console.debug(`DEBUG originalUrl: ${req.originalUrl}`)
+  next()
+})
+app.use(function (req, res, next) {
+  // This needs to be restricted in the future, can't allow random CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.debug(`DEBUG originalUrl: ${req.originalUrl}`)
+  next()
+})
+
+
+app.get('/', function(req, res){
+  res.send("Hello World!");
+});
+
+app.listen(3000);
